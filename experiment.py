beefaaa4b687eed5fc2f7b5db6e04d86cd7a1470
Christopher Schuster <cs@livoris.de>
2014-02-12 21:14:47 -0800

    Fixed regression bug

--- experiment.py
+++ experiment.py
@@ -143,10 +143,6 @@ def decompress(crtifile):
     fo.close()
     return "vase-comp.rti"
 
-def recreate(alpha, beta):
-    crti = compress("vase.rti",30,30)
-    return decompress(crti)
-
 def ploti(img, alpha, beta):
     imgOutMat = img_matrix(img)
     plt.imshow(imgOutMat, interpolation='nearest', hold=True)
@@ -175,7 +171,8 @@ def measure(ucrti, lightx, lighty, alpha, beta):
     return res
 
 def run(alpha, beta):
-    ucrti =  recreate(alpha, beta)
+    crti = compress("vase.rti", alpha, beta)
+    ucrti = decompress(crti)
     res = measure(ucrti, 50.0, 50.0, alpha, beta)
     print("Alpha =",alpha,"Beta =", beta)
     print("SSIM =",res["sim"]),
@@ -183,9 +180,6 @@ def run(alpha, beta):
     print("RMSE =",res["rmse"]),
     print("Comp. Ratio =",res["comp"])
 
-# for alpha in range(20,100,10):
-#     for beta in range(20,100,10):
-#         run(alpha, beta)
-for alpha in [30,60,90]:
-    for beta in [30,60,90]:
+for alpha in range(30,100,30):
+    for beta in range(30,100,30):
         run(alpha, beta)
